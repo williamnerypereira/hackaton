f37eee189114de006ec49fa2f79b2a46b89eafb8
williamnerypereira <william.nery.pereira@gmail.com>
2021-01-23 12:43:26 -0300

    Não estava conseguindo enviar os arquivos, o meu git estava preso no git onde fiz o clone

---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/domains/Card.java
@@ -0,0 +1,27 @@
+package com.orbitallcorp.hack21.cards.domains;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+
+public class Card implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer cardNumber;
+    private String customerName;
+    private Integer documentNumber;
+    private String motherName;
+    private String andress;
+    private String city;
+
+}
